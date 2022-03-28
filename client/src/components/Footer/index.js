2debf3d0581e304e04284108389acc2992f7cf52
pdguerra12 <paul.guerra09@gmail.com>
2022-03-28 15:31:24 -0500

    reaction to thoughts added

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
-	return (
-		<footer className="w-100 mt-auto bg-secondary p-4">
-			<div className="container">&copy;2020 by Paul Guerra✌🏼</div>
-		</footer>
-	);
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">
+        &copy;{new Date().getFullYear()} by Lernantino
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
